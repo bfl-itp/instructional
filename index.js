16ede4eca864a97ae634bb8b91530da04e70860d
Michael Oneppo <moneppo@gmail.com>
2014-09-17 17:36:59 -0400

    First version complete

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 Zepto(function($){
 	var $canvas = $('#canvas');
+	var $parts = $('.part');
 	var ctx = $canvas[0].getContext('2d');
 
 	var none = [];
 	var parts = [];
 
-	var attached = [true,true,true,true,true];
+	var attached = [false,false,false,false,false];
 
-	function mouseMove(e) {
-		var x = (e.pageX - $canvas.offset().left) / $canvas.width();
-		var bucket = Math.floor(x * 5) - 2;
-		if (bucket < -2) bucket = -2;
-		if (bucket > 2) bucket = 2;
+	var bucket = 0;
+	function draw() {
 		ctx.drawImage(none[bucket], 0, 0);
 		for (var i = 0; i < attached.length;i++) {
 			if (attached[i]) {
 				ctx.drawImage(parts[i][bucket], 0,0);
 			}
 		}
+	}
+
+	function mouseMove(e) {
+		var x = (e.pageX - $canvas.offset().left) / $canvas.width();
+		bucket = Math.floor(x * 5) - 2;
+		if (bucket < -2) bucket = -2;
+		if (bucket > 2) bucket = 2;
+		draw();
 	}
 	
 	function loadImages(update, then) {
@@ -59,6 +65,41 @@ Zepto(function($){
 	  },
 	  function() {
 	  	$canvas.on('mousemove', mouseMove);
+	  	draw();
+	  	
+	  	 $parts.draggable({
+        start: function () {
+          this.css('-webkit-transform', 'scale(2)');
+        },
+        stop: function () {
+          this.css('-webkit-transform', 'scale(1)');
+        }
+      });
+
+      $canvas.droppable({
+        drop: function (e, dragEl, dropEl) {
+        	if(dragEl.hasClass('head')) {
+          	attached[0] = true;
+        	}
+
+        	if(dragEl.hasClass('arm')) {
+        		if (attached[1]) {
+          		attached[2] = true;
+          	} else attached[1] = true;
+        	}
+
+        	if(dragEl.hasClass('leg')) {
+        		if (attached[3]) {
+          		attached[4] = true;
+          	} else attached[3] = true;
+        	}
+
+        	dragEl.hide();
+          draw();
+
+          return true;
+        }
+      });
 	  }
 	);
 });
